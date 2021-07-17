b5bcd4bb199d64642ded0f150312ca80822443eb
calintha <cal.intha77@gmail.com>
2021-07-17 18:31:55 +0200

    Add todo

--- oauth-client/Core/Providers/Provider.php
+++ oauth-client/Core/Providers/Provider.php
@@ -9,9 +9,22 @@ abstract class Provider {
         $this->client_id = $client_id;
         $this->client_secret = $client_secret;
     }
-    
+
+    // Get user data with the provider
     function getUser(string $params) {
         $token = $this->getToken($params);
         return $token;
+    }
+
+    // TODO Send the token with the code
+    protected function gettToken(string $params, bool $is_post = false)
+    {
+
+    }
+
+    // TODO function generate
+    public function getAuthorizationUrl()
+    {
+
     }
 }
