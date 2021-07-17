933c62656ea3619c9b7d4ca212a1536905b43bbd
calintha <cal.intha77@gmail.com>
2021-07-17 18:40:28 +0200

    Add getToken

--- oauth-client/Core/Providers/Provider.php
+++ oauth-client/Core/Providers/Provider.php
@@ -3,6 +3,8 @@
 abstract class Provider {
     protected string $client_id;
     protected string $client_secret;
+    protected string $token_url;
+    protected string $redirect_uri;
 
     protected function __construct(string $client_id, string $client_secret)
     {
@@ -10,21 +12,30 @@ abstract class Provider {
         $this->client_secret = $client_secret;
     }
 
-    // Get user data with the provider
-    function getUser(string $params) {
-        $token = $this->getToken($params);
-        return $token;
-    }
-
-    // TODO Send the token with the code
-    protected function gettToken(string $params, bool $is_post = false)
+    // With the params it send the token
+    protected function getToken(string $params, bool $is_post = false)
     {
+        $type = $is_post ? createStreamContext('POST', ['Content-Type: application/x-www-form-urlencoded', 'Content-Length: 0', 'Accept: application/json']) : null;
+        $url = makeUrl($this->token_url, [
+            'params' => $params,
+            'client_id' => $this->client_id,
+            'client_secret' => $this->client_secret,
+            'redirect_uri' => $this->redirect_uri,
+            'grant_type' => 'authorization_code',
+        ]);
 
+        return httpRequest($url, $type)['token'];
     }
 
     // TODO function generate
     public function getAuthorizationUrl()
     {
 
+    }
+
+    // Get user data with the provider
+    function getUser(string $params) {
+        $token = $this->getToken($params);
+        return $token;
     }
 }
