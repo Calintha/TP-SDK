eb5c24548bbda9c3b44f12bb6bf97a12dd73c97d
calintha <cal.intha77@gmail.com>
2021-07-17 18:27:47 +0200

    Add getUser to provider

--- oauth-client/Core/Providers/Provider.php
+++ oauth-client/Core/Providers/Provider.php
@@ -4,21 +4,14 @@ abstract class Provider {
     protected string $client_id;
     protected string $client_secret;
 
+    protected function __construct(string $client_id, string $client_secret)
+    {
+        $this->client_id = $client_id;
+        $this->client_secret = $client_secret;
+    }
+    
     function getUser(string $params) {
-    $result = file_get_contents("http://oauth-server:8081/token?"
-        . "client_id=" . CLIENT_ID
-        . "&client_secret=" . CLIENT_SECRET
-        . "&" . http_build_query($params));
-    $token = json_decode($result, true)["access_token"];
-    // GET USER by TOKEN
-    $context = stream_context_create([
-        'http' => [
-            'method' => "GET",
-            'header' => "Authorization: Bearer " . $token
-        ]
-    ]);
-    $result = file_get_contents("http://oauth-server:8081/me", false, $context);
-    $user = json_decode($result, true);
-    var_dump($user);
-}
+        $token = $this->getToken($params);
+        return $token;
+    }
 }
