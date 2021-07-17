ad1248373965891ab0e39c00a35e6d384457b7cd
calintha <cal.intha77@gmail.com>
2021-07-17 18:23:16 +0200

    Add provider class

---
+++ oauth-client/Core/Providers/Provider.php
@@ -0,0 +1,24 @@
+<?php
+
+abstract class Provider {
+    protected string $client_id;
+    protected string $client_secret;
+
+    function getUser(string $params) {
+    $result = file_get_contents("http://oauth-server:8081/token?"
+        . "client_id=" . CLIENT_ID
+        . "&client_secret=" . CLIENT_SECRET
+        . "&" . http_build_query($params));
+    $token = json_decode($result, true)["access_token"];
+    // GET USER by TOKEN
+    $context = stream_context_create([
+        'http' => [
+            'method' => "GET",
+            'header' => "Authorization: Bearer " . $token
+        ]
+    ]);
+    $result = file_get_contents("http://oauth-server:8081/me", false, $context);
+    $user = json_decode($result, true);
+    var_dump($user);
+}
+}
--- oauth-client/index.php
+++ oauth-client/index.php
@@ -29,25 +29,6 @@ function getAllProviders()
     ];
 }
 
-function getUser($params)
-{
-    $result = file_get_contents("http://oauth-server:8081/token?"
-        . "client_id=" . CLIENT_ID
-        . "&client_secret=" . CLIENT_SECRET
-        . "&" . http_build_query($params));
-    $token = json_decode($result, true)["access_token"];
-    // GET USER by TOKEN
-    $context = stream_context_create([
-        'http' => [
-            'method' => "GET",
-            'header' => "Authorization: Bearer " . $token
-        ]
-    ]);
-    $result = file_get_contents("http://oauth-server:8081/me", false, $context);
-    $user = json_decode($result, true);
-    var_dump($user);
-}
-
 function handleSuccess()
 {
     ["code" => $code, "state" => $state] = $_GET;
